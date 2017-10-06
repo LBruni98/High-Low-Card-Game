d47fd5978133e773d25d9c9301d0325811234590
LBruni98 <31927302+LBruni98@users.noreply.github.com>
2017-10-06 21:22:00 +0100

    Create Player.h

---
+++ Player.h
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class Player {
+  public:
+    string name;
+    double buyin;
+    double starting_buyin;
+  
+  public:
+    Player() {
+      name = "";
+      buyin;
+      starting_buyin;
+    }
+};
