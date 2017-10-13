ab45e86ce830c2da2ea8b28a476181936f1c33eb
LBruni98 <31927302+LBruni98@users.noreply.github.com>
2017-10-13 16:37:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,36 +18,53 @@ using namespace std;
 int cash;
 
 int pickCard() {
-  int card;
-  // srand(time(NULL));
-  
+	//srand(time(NULL)); //Randomizer for both
+	int card;
+	
   // Randomized cards
-  card = rand() % 13 + 1;
-  enum faces {Ace, Jack, Queen, King};
+  card = rand() % 13 + 1; // 
+  //enum faces {Ace, Jack, Queen, King};
+  return card;
 }
 
 // User's input here
-int Guesser() {
+Player Guesser(Player player, int card1, int bet) {
+	int card2 = pickCard();
   string guess; // User's guess is not strewn out into individual characters as in the char function
   
   cout << "Right, Is the next card higher or lower?"; // Displays the messge
-  cin >> guess; //The user types in the guess
+  cin >> guess; // The user types in the guess
   
-  /* THESE IF STATEMENTS WILL BE REDONE SHORTLY */
+  cout <<"================="<< endl; // A E S T H E T I C S
+  cout <<"Your 2nd card: " << card2 << endl; // Second card is printed here
+  cout <<"================="<< endl; // A E S T H E T I C S
+  
+  /* THE PLAYER IS RIGHT OR WRONG*/
   if (guess == "higher" || guess == "Higher") { // The answer is now not case sensitive
-    cout << "That's right! 'Ave a grand!" << endl;
+	   if(card2 > card1){ // This means that the card you're guessing is higher that the first
+	   	cout << "That's right! 'Ave yer bet!" << endl; // If the card value is higher, then the user is right
+	   	player.buyin = player.buyin + bet; //Also, he wins the bet! The lucky bastard!
+	   }else{
+	   	    cout << "Sorry, mate! That's wrong!" << endl; // Otherwise, if the card is NOT higher, he loses  
+	   	    player.buyin = player.buyin - bet; // And his bet is deducted! What a loser
+	   }
+	// Vice Versa
   } else if (guess == "lower" || guess == "Lower") { // See the first if statement
-    cout << "Sorry, mate! That's wrong!" << endl;
+      if(card2 < card1){
+	   	cout << "That's right! 'Ave yer bet!" << endl;
+	   	player.buyin = player.buyin + bet;
+	   }else{
+	   	    cout << "Sorry, mate! That's wrong!" << endl;
+	   	    player.buyin = player.buyin - bet;
+	   }
   } else {
-    cout << "Be clear, mate! I don't understand that!" << endl;
+    cout << "Be clear, mate! I don't understand that!" << endl; // The player inputs anything other that "higher" or "lower"
   }
-  return 0;
+  
+  return player;
 }
 
 int main () {
-  srand(time(NULL)); //Randomizer for both
-  int card1 = pickCard(); // Card1 is gathered from the pickCard function
-  // int card2 = pickCard(); // Leaving out command until I can get the base game working
   double bet; //Betting variable, allows the player to bet
   Player player = Player(); // Player's cash
   
@@ -73,15 +90,19 @@ int main () {
   while (player.buyin > 0) {
     cout << "Right, " << player.name << "! You 'ave £" << player.buyin << "." << endl;
     
-    while (bet == 0 || bet < player.buyin) {
+    do {
       cout << "Now, how much do you want to bet?";
       cin >> bet;
       
       if (bet > player.buyin) {
         cout << "Hey, I'm not a bank! I can't loan you money! Bet within limits!\n";
+      }else{
+      	  cout << endl;
       }
-    }
+    }while (bet == 0 || bet  > player.buyin);
     
+  int card1 = pickCard(); // Card1 is gathered from the pickCard function
+  
   cout << "Cash: £" << player.buyin; // Cash is displayed at the top
   if (player.buyin <= 200) { // The critical amount
     cout << " Watch it! You're almost out of money!"<< endl; // Sends a message if the user has a gambling problem
@@ -90,10 +111,10 @@ int main () {
   }
   cout <<"================="<< endl; // A E S T H E T I C S
   cout <<"Your 1st card: " << card1 << endl; // Where the card value will be displayed
-  // cout <<"Your 2nd card: " << card2 << endl; // Leaving out command until I can get the base game working
   cout <<"================="<< endl; // A E S T H E T I C S
 
-  Guesser();
+  player = Guesser(player, card1, bet);
+  
   }
   return 0;
 }
